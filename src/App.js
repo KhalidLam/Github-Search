c851046197a51db61aedef33e299ab92b57e45c1
Gklam <khalid.lamsadi@outlook.fr>
2020-06-20 12:58:56 +0100

    Add Alert State & Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
 import axios from "axios";
 import "./App.css";
 
@@ -9,19 +10,8 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const resp = await axios.get(
-  //     `https://api.github.com/users?client_id=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-  //     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-
-  //   this.setState({ users: resp.data, loading: false });
-  // }
 
   // Search Github Users
   searchUsers = async (text) => {
@@ -39,15 +29,23 @@ class App extends Component {
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
   render() {
-    const { loading, users } = this.state;
+    const { loading, users, alert } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
+            setAlert={this.setAlert}
             showClear={users.length > 0 ? true : false}
           />
           <Users loading={loading} users={users} />
@@ -58,3 +56,19 @@ class App extends Component {
 }
 
 export default App;
+
+// async componentDidMount() {
+//   this.setState({ loading: true });
+
+//   const resp = await axios.get(
+//     `https://api.github.com/users?client_id=
+//     ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+//     ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+//   );
+
+//   this.setState({ users: resp.data, loading: false });
+// }
+
+//  {alert && (
+//      <button className='btn btn-light btn-block'>{alert.msg}</button>
+//  )}
